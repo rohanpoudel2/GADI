4e7cab11bdab84d4c285cf10f025ab3a65326ec1
Rohan Poudel <nsrapoudel@gmail.com>
2023-01-03 13:09:46 +0545

    Created bigproduct component for individual product page

---
+++ resources/views/components/bigproduct.blade.php
@@ -0,0 +1,19 @@
+<section class="big-product-section">
+    <div class="container">
+        <div class="bigproduct">
+            <div class="product-title">
+                <h1>BMW M4 CSL</h1>
+            </div>
+            <div class="product-image">
+                <img src={{ asset('images/product/01.webp') }} alt="product">
+            </div>
+            <div class="product-desc">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, dolor libero, sequi ex suscipit
+                    inventore voluptatum sed dolorum quisquam aliquid obcaecati consectetur reiciendis ab repellendus
+                    quia officiis voluptate quas quasi.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    @component('components.bigproduct')
+    @endcomponent
+</x-layout>
