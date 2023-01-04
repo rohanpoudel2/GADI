bdbd881fb6d838ea13639b67167090561fc43551
Rohan Poudel <nsrapoudel@gmail.com>
2023-01-04 21:06:13 +0545

    created new models, migration and controller for TestRide

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+
+class FormController extends Controller
+{
+  public function testRideForm(Request $request)
+  {
+
+    $data = $request->input();
+
+    $validator = Validator::make($data, [
+      'name' => 'required|string',
+      'email' => 'required|email',
+      'model' => 'required|string|exists:cars,name'
+    ]);
+
+    if ($validator->fails()) {
+      return redirect('/')
+        ->withErrors($validator)
+        ->withInput();
+    }
+
+    DB::table('test_rides')->insert([
+      'name' => $data['name'],
+      'email' => $data['email'],
+      'model' => $data['model'],
+      'test_ride_date' => DB::raw('DATE_ADD(CURDATE(), INTERVAL 15 DAY)'),
+    ]);
+
+    return redirect('/');
+
+  }
+}
