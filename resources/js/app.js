1638036c10a0950a0e42e4db389458d4662f1b19
Rohan Poudel <nsrapoudel@gmail.com>
2023-01-04 20:20:54 +0545

    added popup form to book test ride

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+
+const ride_button = document.getElementById('ride-button');
+const testRideForm = document.getElementById('test-ride-form');
+const testRideFormCloseButton = document.getElementById('quit-form');
+
+ride_button.addEventListener('click', () => {
+  if (testRideForm.style.display === "none") {
+    testRideForm.style.display = "block";
+  } else {
+    testRideForm.style.display = "none";
+  }
+})
+
+testRideFormCloseButton.addEventListener('click', () => {
+  testRideForm.style.display = "none"
+})
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,8 +12,56 @@
         </div>
         <div class="right">
             <div class="nav-button">
-                <button class="ride-button">Book Test Ride</button>
+                <button class="ride-button" id="ride-button">Book Test Ride</button>
             </div>
         </div>
     </div>
 </nav>
+
+
+@php
+    $formFields = [
+        '
+        <div class="form-item">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" >
+        </div>
+        ',
+    
+        '
+        <div class="form-item">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" >
+        </div>
+        ',
+    
+        '
+        <div class="form-item">
+            <label for="message">Select the car you want to test ride:</label>
+            
+            <select name="selected-car" id="selected-car">
+                <option value="BMW M4 CSL">BMW M3 CSL</option>
+                <option value="BMW M4 CSL">BMW M4 CSL</option>
+                <option value="BMW M4 CSL">BMW M8 CSL</option>
+            </select>
+        </div>
+        ',
+    ];
+@endphp
+
+<div class="test-ride-form" id="test-ride-form" style="display: none">
+    <button class="quit-form" id="quit-form">
+        <i class="fa-solid fa-xmark"></i>
+    </button>
+    @component('components.form', ['formFields' => $formFields])
+        @slot('action')
+            /test-form
+        @endslot
+        @slot('method')
+            POST
+        @endslot
+        @slot('submitText')
+            Book Test Ride
+        @endslot
+    @endcomponent
+</div>
